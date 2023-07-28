7e7a0e5e77fb1cfe0bfa1082353f831ec5058a35
dukbna <doke_the_lofer@yahoo.com>
2023-07-28 17:36:57 +0800

    test button

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -5,7 +5,8 @@ import logoImage from "../../assets/Group 180.png"
 const Logo = () => (
     <div className={css.Logo}>
       <img src={logoImage} alt=""/>
+      <button type="button" class="btn btn-primary">Primary</button>
     </div>
   );
 
-export default Logo;
+export default Logo;  
